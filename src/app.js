505f3ee22932792241dce6e4a7db8f91438e4904
nazar2012 <133564811+nazar2012@users.noreply.github.com>
2025-10-26 11:32:52 +0200

    fix

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// import { add, minus, multiply, divide } from "./js/calculator";
+// import {minus} from "./js/calculator";
+
+
+// console.log(add(5, 10));
+// console.log(minus(15, 10));
+// console.log(multiply(2, 4));
+// console.log(divide(10, 5));
+
+import { nanoid } from 'nanoid';
+import * as calc from "./js/calculator";
+console.log(calc.add(10, 5));
+console.log(calc.minus(10, 5));
+console.log(calc.multiply(10, 5));
+console.log(calc.divide(10, 5));
